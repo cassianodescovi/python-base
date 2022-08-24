7b29273b1e500448d77e0796513c608469def9cb
cassianodescovi <cassianodescovi@gmail.com>
2022-08-24 17:44:43 -0300

    Implementamos o Hello World multi linguas

--- hello.py
+++ hello.py
@@ -30,6 +30,11 @@ if current_language == "pt_BR":
     msg = "Olá Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+   
 
 
 
