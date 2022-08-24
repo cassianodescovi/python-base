069e160e7cbd4b59dafd98f17ab310bce5de9bf8
cassianodescovi <cassianodescovi@gmail.com>
2022-08-24 16:29:33 -0300

    Implementacao hello world

--- hello.py
+++ hello.py
@@ -1,1 +1,38 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+"""
+Hello World Multi LInguas.
+
+Dependendo da lingua configurada no  ambiente o programa exibe a mensagem
+correspondente
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = '0.01'
+__author__ = 'cassianodescovi'
+__license__ = 'Unlicense'
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+
+
+
+print(msg)
